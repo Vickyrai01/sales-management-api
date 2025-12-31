9a157ec8245fd7bfc841dc3bc00af9a3cc8ae0a6
Vicky Rai <vraimondo@frba.utn.edu.ar>
2025-12-30 22:10:30 -0300

    Estructura inicial del proyecto

---
+++ src/main/java/com/github/vickyrai01/salesmanagement/SalesmanagementapiApplication.java
@@ -0,0 +1,13 @@
+package com.github.vickyrai01.salesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesmanagementapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesmanagementapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/vickyrai01/salesmanagement/SalesmanagementapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vickyrai01.salesmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesmanagementapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

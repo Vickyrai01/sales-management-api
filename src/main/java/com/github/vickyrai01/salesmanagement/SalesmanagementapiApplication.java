3958f248befd6ae4e996ca963fcd15362ebc3370
Vicky Rai <vraimondo@frba.utn.edu.ar>
2026-01-10 20:13:35 -0300

    Vuelvo a borrar codigo de mas

--- src/main/java/com/github/vickyrai01/salesmanagement/SalesmanagementapiApplication.java
+++ src/main/java/com/github/vickyrai01/salesmanagement/SalesmanagementapiApplication.java
@@ -1,16 +1,8 @@
 package com.github.vickyrai01.salesmanagement;
 
-import com.github.vickyrai01.salesmanagement.model.auth.Rol;
-import com.github.vickyrai01.salesmanagement.model.auth.User;
-import com.github.vickyrai01.salesmanagement.model.enums.RolName;
-import com.github.vickyrai01.salesmanagement.repository.RolRepository;
-import com.github.vickyrai01.salesmanagement.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import java.util.List;
 
 @SpringBootApplication
 public class SalesmanagementapiApplication {

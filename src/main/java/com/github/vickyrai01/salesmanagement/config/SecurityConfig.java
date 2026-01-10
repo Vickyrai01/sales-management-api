ecf6541a84437c58288dbbf33303e84735c44f5a
Vicky Rai <vraimondo@frba.utn.edu.ar>
2026-01-09 21:07:35 -0300

    Se puede levantar con docker

--- src/main/java/com/github/vickyrai01/salesmanagement/config/SecurityConfig.java
+++ src/main/java/com/github/vickyrai01/salesmanagement/config/SecurityConfig.java
@@ -13,13 +13,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.ArrayList;
@@ -78,8 +73,8 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    /*
-    public static void main(String[] args) {
+
+/*    public static void main(String[] args) {
         System.out.println(new BCryptPasswordEncoder().encode("2003"));
     }
     */
